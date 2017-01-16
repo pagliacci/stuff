7dff70bf75321aaa55241b7d5f92e27353498716
Anton <ostrokonskiy@gmail.com>
2017-01-16 15:22:34 +0300

    Update testtest.js

--- testtest.js
+++ testtest.js
@@ -1,29 +1,6 @@
-var f = document.createElement("form");
-f.setAttribute('method',"post");
-f.setAttribute('action',"submit.php");
+var e1 = document.CreateElement("input");
+el.type = "text";
+el.name = "myformvar";
 
-//create input element
-var i = document.createElement("input");
-i.type = "text";
-i.name = "user_name";
-i.id = "user_name1";
-
-//create a checkbox
-var c = document.createElement("input");
-c.type = "checkbox";
-c.id = "checkbox1";
-c.name = "check1";
-
-//create a button
-var s = document.createElement("input");
-s.type = "submit";
-s.value = "Submit";
-
-// add all elements to the form
-f.appendChild(i);
-f.appendChild(c);
-f.appendChild(s);
-
-// add the form inside the body
-$("body").append(f);   //using jQuery or
-document.getElementsByTagName('body')[0].appendChild(f); //pure javascript
+var cont = document.GetElementById("formcontent")
+cont.appendChild(e1);
