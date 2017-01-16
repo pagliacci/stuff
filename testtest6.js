56b7c5fee0f843df05258c3ec03a3e468d55415e
Anton <ostrokonskiy@gmail.com>
2017-01-16 16:02:49 +0300

    Update and rename testtest5.js to testtest6.js

--- testtest6.js
+++ testtest6.js
@@ -2,5 +2,6 @@ alert(1);
 
 var btn = document.createElement("BUTTON");        // Create a <button> element
 var t = document.createTextNode("CLICK ME");       // Create a text node
+var x = document.getElementsByClassName("blueBtn payBlueBtn")[0];
 btn.appendChild(t);                                // Append the text to <button>
-document.body.insertBefore(btn, document.body.childNodes[1]);
+document.body.insertBefore(btn, x);
