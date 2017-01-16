eb48c33187934a2cdda91d177bfb5bbe0c341adc
Anton <ostrokonskiy@gmail.com>
2017-01-16 16:26:00 +0300

    Update testtest12.js

--- testtest12.js
+++ testtest12.js
@@ -1,5 +1,3 @@
-alert(1);
-
 var btn = document.createElement("BUTTON");        // Create a <button> element
 var t = document.createTextNode("CLICK ME");       // Create a text node
 var x = document.getElementsByClassName("blueBtn payBlueBtn")[0];
