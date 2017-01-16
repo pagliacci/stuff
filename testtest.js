5c06bfb305011e1ef66a9474347f4e83468505a5
Anton <ostrokonskiy@gmail.com>
2017-01-16 15:38:15 +0300

    Update testtest.js

--- testtest.js
+++ testtest.js
@@ -1,11 +1,1 @@
-var foo = {
-  createElement: function(tagname) {
-    if (this._secretvarthatisneeded) {
-      console.log(tagname + " Element Created!");
-    }
-  },
-  _secretvarthatisneeded: true
-}
-
-var bar = foo.createElement;
-bar("bar");
+alert(1);
