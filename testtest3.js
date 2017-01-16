5f871b651553b1974ce771894027932c40406f0c
Anton <ostrokonskiy@gmail.com>
2017-01-16 15:49:54 +0300

    Create testtest3.js

---
+++ testtest3.js
@@ -0,0 +1,6 @@
+alert(1);
+
+var btn = document.createElement("BUTTON");        // Create a <button> element
+var t = document.createTextNode("CLICK ME");       // Create a text node
+btn.appendChild(t);                                // Append the text to <button>
+document.body.insertBefore(btn, document.body.childNodes[0]);
