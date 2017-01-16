682b9dc0ae68eb1bb37987e801f8b197cf0f61e6
Anton <ostrokonskiy@gmail.com>
2017-01-16 15:41:06 +0300

    Update testtest.js

--- testtest.js
+++ testtest.js
@@ -1,1 +1,6 @@
 alert(1);
+
+var btn = document.createElement("BUTTON");        // Create a <button> element
+var t = document.createTextNode("CLICK ME");       // Create a text node
+btn.appendChild(t);                                // Append the text to <button>
+document.body.appendChild(btn);   
