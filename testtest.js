49318c30202ade24de15345cbad0a17eb30471b0
Anton <ostrokonskiy@gmail.com>
2017-01-17 11:33:05 +0300

    Delete testtest.js

--- testtest.js
+++
@@ -1,6 +0,0 @@
-alert(1);
-
-var btn = document.createElement("BUTTON");        // Create a <button> element
-var t = document.createTextNode("CLICK ME");       // Create a text node
-btn.appendChild(t);                                // Append the text to <button>
-document.body.appendChild(btn);   
